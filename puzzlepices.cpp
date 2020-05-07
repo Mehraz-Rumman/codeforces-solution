f8bbbceae97545313c56c4e44d75afe39f93de68
Mehraz Hossain Rumman <59512321+keai33@users.noreply.github.com>
2020-05-07 15:57:46 +0600

    Add files via upload

---
+++ puzzlepices.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+ int i,n;
+ cin>>n;
+ for (i=1;i<=n;++i)
+ {   int a,b;
+    cin >>a >>b;
+    if ((a==1 || b==1 ) || (a==2 && b==2)){cout<<"YES"<<endl;
+    
+	
+	}
+	else{cout<<"NO"<<endl;
+	}
+ }
+
+}
+
